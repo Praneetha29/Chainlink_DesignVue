e8411c7c8394728936f26b29eac16f0feae21784
Gourav Mohanty <gouravmohanty2001@gmail.com>
2023-12-09 09:26:34 +0530

    feat: Upload Contracts added

--- eth-template/src/components/Upload/index.js
+++ eth-template/src/components/Upload/index.js
@@ -22,43 +22,107 @@ import {
 
 } from './UploadElements';
 import Navbar from '../Navbar';
-Upload = () => {
- 
+import lighthouse from '@lighthouse-web3/sdk';
+import { ethers } from 'ethers';
+//API: 7c85bd02.2b99f4e9253346b3ab955d627c237a29
+
+const Upload = ({marketplace, nft, account}) => {
+  const [uploadedFile, setUploadedFile] = useState(null);
+  // const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState("No image selected");
+  const [image, setImage] = useState("");
+  const [Chain, setChain] = useState('');
+  const [DesignType, setDesignType] = useState('');
+  const [files, setfiles] = useState(null);
+
+  const handleChainChange = (e) => {
+    setChain(e.target.value);
+  };
+
+  const handleDesignTypeChange = (e) => {
+    setDesignType(e.target.value);
+  };
+
+  const handleFileChange = (e) => {
+    setfiles(e.target.files[0]);
+  };
+
+  const changeToInt=(_x)=>{
+    const x= ethers.utils.formatEther(_x)(10*18);
+    return x;
+}
+
+
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0];
+    setUploadedFile(file);
+  }, []);
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
- 
+  const progressCallback = (progressData) => {
+    let percentageDone =
+      100 - (progressData?.total / progressData?.uploaded)?.toFixed(2);
+    console.log(percentageDone);
+  };
+
+  let imh="";
+  
+  const uploadFile = async(file) =>{
+    const output = await lighthouse.upload(file, "7c85bd02.2b99f4e9253346b3ab955d627c237a29", false, null, progressCallback);
+    console.log('File Status:', output);
+    imh=output.data.Hash;
+    console.log('Visit at https://gateway.lighthouse.storage/ipfs/' + output.data.Hash);
+  }
+
+  const handleUploadClick = async () => {
+    const chain = document.getElementById("Chain").value;
+    const designType = document.getElementById("DesignType").value;
+    if(files){
+      const output = await lighthouse.upload(files, "7c85bd02.2b99f4e9253346b3ab955d627c237a29", false, null, progressCallback);
+      console.log('File Status:', output);
+      imh=output.data.Hash;
+      console.log('Visit at https://gateway.lighthouse.storage/ipfs/' + output.data.Hash);
+    }
+    let hash=imh;
+    const tx=await marketplace.addDesign(chain, designType, hash);
+    await tx.wait();
+    console.log("Transaction Hash: ", tx.hash);
+    alert("Transaction Hash: "+tx.hash);
+  };
+
+
   
 
   return (
     <>
       <SplitContainer>
         <LeftContainer>
-        <account>Account:</account>
+        <account>Account:{account}</account>
           <Form>
            
            
               <InputContainer>
-              <Text >Chain:</Text>
-              <StyledInput type="number" id="Chain"  />
+              <Text htmlFor="Chain">Chain:</Text>
+              <StyledInput type="number" id="Chain" value={Chain} onChange={handleChainChange} />
             </InputContainer>
             <InputContainer>
               <Text htmlFor="DesignType">Design Type:</Text>
-              <StyledInput type="number" id="DesignType" />
+              <StyledInput type="number" id="DesignType" value={DesignType} onChange={handleDesignTypeChange} />
               </InputContainer>
               <InputContainer>
       <Text>Upload Image:</Text>
-      <StyledInput  />
+      <StyledInput onChange={e => setfiles(e.target.files)} type="file" {...getRootProps()} />
     </InputContainer>
 
-            <FormButton type="button"  >Upload File</FormButton>
+            <FormButton type="button" onClick={handleUploadClick} >Upload File</FormButton>
           </Form>
         </LeftContainer>
         <RightContainer>
           <UploadedImageContainer>
             {uploadedFile ? (
               <>
-                <UploadedImage alt="Uploaded" />
+                <UploadedImage src={URL.createObjectURL(uploadedFile)} alt="Uploaded" />
                 <UploadedImageText>Your design has been uploaded ✔️</UploadedImageText>
               </>
             ) : (
